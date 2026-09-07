4226d6044d25760834f5f46ea34f9650b3b9e967
code-with-maheswar <mannamaheshwar51@gmail.com>
2026-09-07 16:53:34 +0530

    setup

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import httpStatus from 'http-status';
+import mongoose from 'mongoose';
+dotenv.config();
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/in', (req, res) => {
+  res.status(httpStatus.OK).send({ name: 'rahul', data: 'kam chor' });
+});
+
+let start = async () => {
+  const con = await mongoose.connect(process.env.MONGO_URL);
+  console.log(`result of db ${con.connection.name}`);
+  server.listen(process.env.PORT || 8080, () => {});
+};
+
+start();
