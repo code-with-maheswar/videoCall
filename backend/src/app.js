c5b4e980a2ca6380bc0246e1ce3a4c0b3aef191e
code-with-maheswar <mannamaheshwar51@gmail.com>
2026-09-09 15:25:15 +0530

    Add user authentication and meeting models with routes
    - Implement user login and registration functionality
    - Create Meeting and User models
    - Set up user routes for login and registration
    - Integrate socket connection management
    - Update package dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,19 +5,25 @@ import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import httpStatus from 'http-status';
 import mongoose from 'mongoose';
+import userRoutes from './routes/users.routes.js';
+import { connectToSocket } from './cotrollers/socketManager.js';
 dotenv.config();
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = connectToSocket(server);
 
-app.get('/in', (req, res) => {
-  res.status(httpStatus.OK).send({ name: 'rahul', data: 'kam chor' });
-});
+app.set('port', process.env.PORT || 8080);
+app.use(cors({ credentials: true, origin: '*' }));
+app.use(express.json({ limit: '40kb' }));
+app.use(express.urlencoded({ limit: '40kb', extended: true }));
+app.use('/api/v1/users', userRoutes);
 
 let start = async () => {
   const con = await mongoose.connect(process.env.MONGO_URL);
   console.log(`result of db ${con.connection.host}`);
-  server.listen(process.env.PORT || 8080, () => {});
+  server.listen(app.get('port'), () => {
+    console.log(`port is working to ${process.env.PORT}`);
+  });
 };
 
 start();
